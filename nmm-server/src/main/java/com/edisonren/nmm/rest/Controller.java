4f9e75f997e28fc9813504eda26152392d3938c5
Edison Ren <edison.z.ren@gmail.com>
2017-09-03 14:59:09 -0700

    integrate NmmService in controller and enable logging

--- nmm-server/src/main/java/com/edisonren/nmm/rest/Controller.java
+++ nmm-server/src/main/java/com/edisonren/nmm/rest/Controller.java
@@ -1,8 +1,11 @@
 package com.edisonren.nmm.rest;
 
+import com.edisonren.nmm.service.NmmService;
 import com.edisonren.nmm.v1.NmmRequest;
 import com.edisonren.nmm.v1.NmmResponse;
 import com.edisonren.nmm.v1.Scenario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,12 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
+import java.util.List;
+
 /**
  * Created by edison on 8/26/17.
  */
 @RestController
 @RequestMapping(value = "/nmm/v1")
 public class Controller {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Inject private NmmService nmmService;
 
     @RequestMapping(value = "/health-check",
             produces = {MediaType.TEXT_PLAIN_VALUE},
@@ -32,23 +41,36 @@ public class Controller {
 
     }
 
+    /**
+     * Save the association of scenario and static response
+     * , and publish it to message bus
+     */
     @RequestMapping(value = "/", method = RequestMethod.POST,
             produces = {MediaType.APPLICATION_JSON_VALUE},
             consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public NmmResponse postNmmRequest(@RequestBody NmmRequest request) {
-        return null;
+    public NmmResponse processNmmRequest(@RequestBody NmmRequest request) {
+        logger.info("process NmmRequest");
+        return nmmService.processNmmRequest(request);
     }
 
-    @RequestMapping(value = "/view", method = RequestMethod.POST,
+    /**
+     * For given Scenario, return all matched Responses.
+     *
+     * @param scenario payload and parameters are not used to evaluate the scenario.
+     * @return list of matched NmmResponse, whose scenario matches the given one.
+     */
+    @RequestMapping(value = "/match", method = RequestMethod.POST,
             produces = {MediaType.APPLICATION_JSON_VALUE},
             consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public NmmResponse matchNmmResponse(@RequestBody Scenario senario) {
-        return null;
+    public List<NmmResponse> matchScenario(@RequestBody Scenario scenario) {
+        logger.info("match scenario");
+        return nmmService.getResponsesByScenario(scenario);
     }
 
-    @RequestMapping(value = "/{scenarioId}", method = RequestMethod.GET,
+    @RequestMapping(value = "/match/{scenarioId}", method = RequestMethod.GET,
             produces = {MediaType.APPLICATION_JSON_VALUE})
     public NmmResponse getNmmResponseByLoanId(@PathVariable("scenarioId") String scenarioId) {
-        return null;
+        logger.info("match scenario by id");
+        return nmmService.getResponseByScenarioId(scenarioId);
     }
 }
