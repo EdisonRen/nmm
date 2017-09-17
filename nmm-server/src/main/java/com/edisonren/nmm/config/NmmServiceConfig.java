fc86fce1710da972e6c4d4b0bd5d33ace68858da
Edison Ren <edison.z.ren@gmail.com>
2017-09-17 13:24:27 -0700

    put NmmModelRepo in NmmService for testing run

--- nmm-server/src/main/java/com/edisonren/nmm/config/NmmServiceConfig.java
+++ nmm-server/src/main/java/com/edisonren/nmm/config/NmmServiceConfig.java
@@ -1,15 +1,18 @@
 package com.edisonren.nmm.config;
 
+import com.edisonren.nmm.Config.DaoConfig;
 import com.edisonren.nmm.service.NmmService;
 import com.edisonren.nmm.service.NmmServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * Created by edison on 9/3/17.
  */
 @Configuration
+@Import({DaoConfig.class})
 //@EnableAspectJAutoProxy(proxyTargetClass = true)
 @ComponentScan(value = {"com.edisonren.nmm"})
 public class NmmServiceConfig {
--- nmm-server/src/main/java/com/edisonren/nmm/service/NmmServiceImpl.java
+++ nmm-server/src/main/java/com/edisonren/nmm/service/NmmServiceImpl.java
@@ -1,10 +1,14 @@
 package com.edisonren.nmm.service;
 
+import com.edisonren.nmm.dao.NmmModelRepository;
+import com.edisonren.nmm.model.NmmModel;
 import com.edisonren.nmm.v1.NmmRequest;
 import com.edisonren.nmm.v1.NmmResponse;
 import com.edisonren.nmm.v1.Scenario;
+import com.edisonren.nmm.v1.ScenarioInfo;
 import com.edisonren.nmm.validator.NmmRequestValidator;
 import com.edisonren.nmm.validator.ScenarioValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
@@ -15,9 +19,21 @@ import java.util.List;
  */
 public class NmmServiceImpl implements NmmService {
 
+    @Autowired
+    private NmmModelRepository modelRepository;
+
     @Override
     public NmmResponse processNmmRequest(NmmRequest nmmRequest) {
         NmmRequestValidator.validate(nmmRequest);
+
+        NmmModel model = new NmmModel.NmmModelBuilder()
+                .setMockVer(0)
+                .setScenario(nmmRequest.getScenario())
+                .setScenarioInfo(new ScenarioInfo())    // TODO: this is a dummy one!
+                .setResponse(nmmRequest.getResponse())
+                .build();
+
+        modelRepository.saveNmmModel(model);
 
         return new NmmResponse();
     }
