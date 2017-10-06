4dc1b47101eb1abeb5f73b136bcd168cac52d3cb
Edison Ren <edison.z.ren@gmail.com>
2017-10-05 23:31:20 -0700

    move redis config to application.properties

--- nmm-dao/src/main/java/com/edisonren/nmm/Config/DaoConfig.java
+++ nmm-dao/src/main/java/com/edisonren/nmm/Config/DaoConfig.java
@@ -1,8 +1,11 @@
 package com.edisonren.nmm.Config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -11,10 +14,11 @@ import org.springframework.data.redis.core.RedisTemplate;
  */
 @Configuration
 public class DaoConfig {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Value("${HOST_NAME:localhost}")
+    @Value("${REDIS_HOST_NAME}")
     private String hostName;
-    @Value("${PORT:6379}")
+    @Value("${REDIS_PORT}")
     private Integer port;
 
     @Bean
@@ -31,6 +35,7 @@ public class DaoConfig {
     public JedisConnectionFactory jedisConnectionFactory() {
         // TODO: add Jedis sentinel to ConnFactor for monitor, notification and failover
         // TODO: use pool
+        logger.info("NMM_REDIS starting connection on {}:{}", hostName, port);
         JedisConnectionFactory jedisConnFactory = new JedisConnectionFactory();
         jedisConnFactory.setHostName(hostName);
         jedisConnFactory.setPort(port);
