9dd89176bfbc7f380ade7bb6ce1f961e9311057a
Edison Ren <edison.z.ren@gmail.com>
2017-10-09 21:43:47 -0700

    initialize nmm client

---
+++ nmm-client/src/main/java/com/edisonren/nmm/NmmMonitor.java
@@ -0,0 +1,18 @@
+package com.edisonren.nmm;
+
+import com.edisonren.nmm.v1.NmmModel;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by edison on 10/9/17.
+ */
+@Aspect
+@Component
+public class NmmMonitor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NmmMonitor.class);
+
+
+}
---
+++ nmm-client/src/test/java/com/edisonren/nmm/NmmMonitorTest.java
@@ -0,0 +1,7 @@
+package com.edisonren.nmm;
+
+/**
+ * Created by edison on 10/9/17.
+ */
+public class NmmMonitorTest {
+}
